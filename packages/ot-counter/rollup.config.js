7dd106432ec6e4b8cd5e2a5c278753a783eac741
WindrunnerMax <651525974@qq.com>
2023-01-22 23:09:15 +0800

    fix: jsx-runtime error

--- packages/ot-counter/rollup.config.js
+++ packages/ot-counter/rollup.config.js
@@ -1,10 +1,15 @@
-import ts from "rollup-plugin-typescript2";
+import ts from "@rollup/plugin-typescript";
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import path from "path";
 import terser from "@rollup/plugin-terser";
 import postcss from "rollup-plugin-postcss";
+
+process.env.NODE_ENV === "production";
+const banner = `var process = {
+  env: { NODE_ENV: 'production' }
+};`;
 
 export default async () => {
   return {
@@ -16,13 +21,16 @@ export default async () => {
         "react": "React",
         "react-dom": "ReactDOM",
       },
+      banner,
     },
     plugins: [
       resolve(),
-      commonjs({ include: "node_modules/**" }),
+      commonjs({
+        include: /node_modules/, // `monorepo regexp`
+      }),
+      postcss({ minimize: true, extensions: [".css", ".scss"] }),
       babel({
-        extensions: ["js", "jsx", "ts", "tsx"],
-        exclude: "node_modules/**",
+        exclude: /node_modules/,
         babelHelpers: "bundled",
         presets: [
           ["@babel/preset-react"],
@@ -31,9 +39,7 @@ export default async () => {
       }),
       ts({
         tsconfig: path.resolve(__dirname, "./tsconfig.json"),
-        extensions: [".ts", ".tsx"],
       }),
-      postcss({ minimize: true, extensions: [".css", ".scss"] }),
       terser(),
     ],
     external: ["react", "react-dom"],
