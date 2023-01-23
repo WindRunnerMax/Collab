c08b6e85e8c66ea5d6f4ed4b64711aeb5e3f4b74
WindrunnerMax <651525974@qq.com>
2023-01-23 09:51:27 +0800

    fix: resolve jsx-runtime production

--- packages/ot-counter/rollup.config.js
+++ packages/ot-counter/rollup.config.js
@@ -3,21 +3,40 @@ import commonjs from "@rollup/plugin-commonjs";
 import path from "path";
 import postcss from "rollup-plugin-postcss";
 import esbuild from "rollup-plugin-esbuild";
+import replacement from "rollup-plugin-module-replacement";
 
 process.env.NODE_ENV === "production";
 const banner = `var process = {
   env: { NODE_ENV: 'production' }
 };`;
 
+const root = path.resolve(__dirname);
 export default async () => {
   return {
     input: "src/index.tsx",
     output: {
       dir: "./build",
-      format: "es",
+      format: "iife",
       banner,
+      globals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+      },
     },
     plugins: [
+      replacement({
+        entries: [
+          {
+            find: "react/jsx-runtime",
+            replacement: () => {
+              return path.resolve(
+                root,
+                "node_modules/react/cjs/react-jsx-runtime.production.min.js"
+              );
+            }, // production react/jsx-runtime
+          },
+        ],
+      }),
       resolve(),
       commonjs({
         include: /node_modules/, // `monorepo regexp`
@@ -31,5 +50,6 @@ export default async () => {
         tsconfig: path.resolve(__dirname, "tsconfig.json"),
       }),
     ],
+    external: ["react", "react-dom"],
   };
 };
