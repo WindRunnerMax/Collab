7f4560606e112a09d549f3707b474ae06729a3c6
WindrunnerMax <651525974@qq.com>
2023-01-25 15:26:04 +0800

    feat: bind doc by shared connection

--- packages/ot-counter/src/client.ts
+++ packages/ot-counter/src/client.ts
@@ -4,9 +4,8 @@ import { Socket } from "sharedb/lib/sharedb";
 
 export type ClientCallback = (num: number) => void;
 
-export default class Connection {
+class Connection {
   private connection: sharedb.Connection;
-  private doc: sharedb.Doc<{ num: number }> | null = null;
 
   constructor() {
     // Open WebSocket connection to ShareDB server
@@ -14,11 +13,10 @@ export default class Connection {
     this.connection = new sharedb.Connection(socket as Socket);
   }
 
-  bindDoc(cb: ClientCallback) {
+  bind(cb: ClientCallback) {
     // Create local Doc instance mapped to 'examples' collection document with id 'counter'
     // collectionName, documentID
     const doc = this.connection.get("ot-example", "counter");
-    this.doc = doc;
     // Get initial value of document and subscribe to changes
     const onSubscribe = () => cb(doc.data.num);
     doc.subscribe(onSubscribe);
@@ -26,23 +24,26 @@ export default class Connection {
     // update the number on the page
     const onOpExec = () => cb(doc.data.num);
     doc.on("op", onOpExec);
-    return () => {
-      doc.off("op", onOpExec);
-      doc.unsubscribe(onSubscribe);
+    return {
+      // When clicking on the '+1' button, change the number in the local
+      // document and sync the change to the server and other connected
+      // clients
+      increase: () => {
+        // Increment `doc.data.num`. See
+        // https://github.com/ottypes/json0 for list of valid operations.
+        doc.submitOp([{ p: ["num"], na: 1 }]);
+      },
+      unbind: () => {
+        doc.off("op", onOpExec);
+        doc.unsubscribe(onSubscribe);
+        doc.destroy();
+      },
     };
   }
 
-  // When clicking on the '+1' button, change the number in the local
-  // document and sync the change to the server and other connected
-  // clients
-  increment() {
-    // Increment `doc.data.num`. See
-    // https://github.com/ottypes/json0 for list of valid operations.
-    this.doc && this.doc.submitOp([{ p: ["num"], na: 1 }]);
-  }
-
   destroy() {
-    this.doc && this.doc.destroy();
     this.connection.close();
   }
 }
+
+export default new Connection();
--- packages/ot-counter/src/counter.tsx
+++ packages/ot-counter/src/counter.tsx
@@ -1,20 +1,20 @@
-import { FC, useEffect, useState } from "react";
-import Connection from "./client";
-
-const connection = new Connection();
+import { FC, useEffect, useRef, useState } from "react";
+import connection from "./client";
 
 export const Counter: FC = () => {
   const [count, setCount] = useState(0);
+  const op = useRef<(() => void) | null>(null);
 
   useEffect(() => {
-    const unBindDoc = connection.bindDoc(setCount);
-    return () => unBindDoc();
+    const { unbind, increase } = connection.bind(setCount);
+    op.current = increase;
+    return () => unbind();
   }, []);
 
   return (
     <>
       <div>Count: {count}</div>
-      <button onClick={() => connection.increment()}>+1</button>
+      <button onClick={() => op.current && op.current()}>+1</button>
     </>
   );
 };
