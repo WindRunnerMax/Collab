54c6c5940308135de32930ecd23461c1b196f9e9
WindrunnerMax <651525974@qq.com>
2023-01-26 15:54:18 +0800

    chore: config

--- packages/config/index.js
+++ packages/config/index.js
@@ -1,8 +1,8 @@
+const CDN_HOST = "https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/";
+
 module.exports = {
   CDN: {
-    REACT:
-      "https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/react/17.0.2/umd/react.production.min.js",
-    REACT_DOM:
-      "https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/react-dom/17.0.2/umd/react-dom.production.min.js",
+    REACT: CDN_HOST + "react/17.0.2/umd/react.production.min.js",
+    REACT_DOM: CDN_HOST + "react-dom/17.0.2/umd/react-dom.production.min.js",
   },
 };
