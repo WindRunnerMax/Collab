2e7618787ac6b4a853d77e0aace21d27abcadd7c
WindrunnerMax <651525974@qq.com>
2023-03-04 15:47:29 +0800

    chore: express static resources

--- packages/crdt-counter/server/index.ts
+++ packages/crdt-counter/server/index.ts
@@ -1,6 +1,12 @@
 import { exec } from "child_process";
+import express from "express";
 
 // https://github.com/yjs/y-webrtc/blob/master/bin/server.js
 exec("PORT=3001 npx y-webrtc-signaling", (err, stdout, stderr) => {
   console.log(stdout, stderr);
 });
+
+const app = express();
+app.use(express.static("build"));
+app.listen(3000);
+console.log("Listening on http://localhost:3000");
